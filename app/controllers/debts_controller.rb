3efbf8c09b47edcb2f00a61fa97cadc7cf22360d
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2026-05-10 23:37:43 -0300

    feat: add debt management functionality

---
+++ app/controllers/debts_controller.rb
@@ -0,0 +1,47 @@
+class DebtsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_debt, only: [:edit, :update, :destroy]
+  layout "dashboard"
+
+  def index
+    @debts = current_user.debts
+  end
+
+  def new
+    @debt = current_user.debts.new
+  end
+
+  def create
+    @debt = current_user.debts.new(debt_params)
+    if @debt.save
+      redirect_to dashboard_debts_path, notice: "Dívida cadastrada com sucesso."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @debt.update(debt_params)
+      redirect_to dashboard_debts_path, notice: "Dívida atualizada."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @debt.destroy
+    redirect_to dashboard_debts_path, notice: "Dívida removida."
+  end
+
+  private
+
+  def set_debt
+    @debt = current_user.debts.find(params[:id])
+  end
+
+  def debt_params
+    params.require(:debt).permit(:name, :total_amount, :monthly_payment, :installments_count, :remaining_installments)
+  end
+end
